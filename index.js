301d9ed993ac343e87caac314b4baaf8e5316127
jconway4 <jconwa4@illinois.edu>
2018-03-14 22:38:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var takeANumber = function(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 };
+
+var nowServing = function(katzDeliLine) {
+  
+};
