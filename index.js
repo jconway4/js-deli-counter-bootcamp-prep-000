cfe0d95d53f32629e3660da1753d7afd33e1b222
jconway4 <jconwa4@illinois.edu>
2018-03-14 22:49:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ var currentLine = function(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
+    let i = 0;
     do {
       
     } while (i < katzDeliLine.length);
